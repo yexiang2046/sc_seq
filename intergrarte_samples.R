63c22b7092fc37aeff7258781f7038b69a13e70d
yexiang2046 <yexiang2046@gmail.com>
2020-06-13 15:47:11 -0500

    first commit

---
+++ intergrarte_samples.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+library(Seurat)
+
+
+
+
+args = commandArgs(trailingOnly=TRUE)
+
+
+
+# test if there is at least two argument: if not, return an error
+if (length(args) > 1) {
+  stop("At least two arguments must be supplied (input file).n", call.=FALSE)
+} 
+
+
+outfile = "integrated_seurat.rds"
+
+samples = list(length(args))
+samples_s = list(length(args))
+for i in seq_along(args){
+	samples[i] <- Read10x(data.dir = args[i], gene.column = 2, unique.features = TRUE)
+	samples_s[i] <- CreateSeuratObject(samples[i], project = "Lupus_CD4", min.cells = 3, min.features = 500)
+	samples_s[i] <- NormalizeData(samples_s[i], verbose = TRUE)
+	samples_s[i] <- FindVariableFeatures(samples_s[i], select.method = "vst", nfeatures = 2000, verbose = FALSE)
+}
+
+
+
+
+archors_cca <- FindIntegrationAnchors(samples_s, dims = 1:30)
+
+samples_integrated <- IntegrateData(samples_s)
+
+saveRDS(samples_integrated, outfile)
+
